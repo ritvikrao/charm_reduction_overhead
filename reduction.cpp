a465ce01f37246b3a738eac4efe7eb518fe594c9
Ritvik Rao <rrao@login03.frontier.olcf.ornl.gov>
2024-08-08 15:12:57 -0400

    no reduction vs reduction

--- reduction.cpp
+++ reduction.cpp
@@ -9,7 +9,12 @@ CProxy_ReduceChares arr;
 
 void start_reductions(void *obj, double time)
 {
-    arr.contribute_reduction(0);
+    arr.contribute_reduction();
+}
+
+void end_time(void *obj, double time)
+{
+    arr.contribute_comp_counts();
 }
 
 class Main : public CBase_Main
@@ -17,6 +22,7 @@ class Main : public CBase_Main
     private:
     std::vector<double> reduction_times;
     double last_time;
+    double begin_time;
 
     public:
     Main(CkArgMsg *m)
@@ -24,7 +30,9 @@ class Main : public CBase_Main
         mainProxy = thisProxy;
         arr = CProxy_ReduceChares::ckNew(CkNumPes());
         last_time = CkWallTimer() + (0.1/1000);
-        CcdCallFnAfter(start_reductions, (void *) this, 0.1);
+        //CcdCallFnAfter(start_reductions, (void *) this, 0.1);
+        CcdCallFnAfter(end_time, (void *) this, 5000.0);
+        begin_time = CkWallTimer();
         arr.self_computation();
     }
     void reduce()
@@ -41,19 +49,29 @@ class Main : public CBase_Main
                 time_sum += reduction_times[i];
             }
             ckout << "Average reduction time: " << time_sum / 100.0 << endl;
-            CkExit(0);
+            arr.contribute_comp_counts();
         }
+    }
+    void end(long counts)
+    {
+        double time_elapsed = CkWallTimer() - begin_time;
+        ckout << "Computations per second: " << counts / time_elapsed << endl;
+        ckout << "Seconds per computation: " << time_elapsed / counts << endl;
+        CkExit(0);
     }
 };
 
 class ReduceChares : public CBase_ReduceChares
 {
+    private:
+    long comp_counts = 0;
 
     public:
     ReduceChares(){
     }
     void self_computation()
     {
+        comp_counts++;
         usleep(10);
         arr[thisIndex].self_computation();
     }
@@ -65,6 +83,11 @@ class ReduceChares : public CBase_ReduceChares
     void broadcast()
     {
         contribute_reduction();
+    }
+    void contribute_comp_counts()
+    {
+        CkCallback cb(CkReductionTarget(Main, end), mainProxy);
+        contribute(sizeof(long),&comp_counts,CkReduction::sum_long, cb);
     }
 
 };
